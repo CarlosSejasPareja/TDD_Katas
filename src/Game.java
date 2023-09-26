9760b266ad3b4f9885c0acc0cb8598517945cf18
Carlos Sejas <carlos.sejas@ucb.edu.bo>
2023-09-26 01:50:07 -0400

    refactoring , mejorando condicional de strike llevando a una funcion

--- src/Game.java
+++ src/Game.java
@@ -10,7 +10,7 @@ public class Game {
         int score=0;
         int frameIndex=0;
         for(int frame=0;frame<10;frame++){
-            if(rolls[frameIndex]==10){
+            if(isStrike(frameIndex)){
                score+=10+rolls[frameIndex+1]+rolls[frameIndex+2];
                frameIndex++; 
             }else if(isSpare(frameIndex)){
@@ -26,6 +26,9 @@ public class Game {
     }
     private boolean isSpare(int frameIndex){
         return rolls[frameIndex]+rolls[frameIndex+1]==10;
+    }
+    private boolean isStrike(int frameIndex){
+        return rolls[frameIndex]==10;
     }
 
 }
