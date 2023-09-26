dbb37f5f98603ff24a15c81616a47128c20044d2
Carlos Sejas <carlos.sejas@ucb.edu.bo>
2023-09-26 01:31:48 -0400

    mejora de disenio en la clase Game

--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -15,11 +15,17 @@ public class BowlingGameTest extends TestCase {
         rollMany(20, 1);
         assertEquals(20, g.score());
     }
+    public void testeOneSpare() throws Exception{
+        g.roll(5);
+        g.roll(5);
+        g.roll(3);
+        rollMany(17, 0);
+        assertEquals(16, g.score());
+    }
      private void rollMany(int n, int pins){
         for(int i=0;i<n;i++){
             g.roll(pins);
         }
-
      }
 
 
