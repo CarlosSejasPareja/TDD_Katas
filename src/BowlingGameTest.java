f3dbadfb655779722bf0778fddf97d9cd08a9bcf
Carlos Sejas <carlos.sejas@ucb.edu.bo>
2023-09-26 01:18:57 -0400

    refactoring de creacion de nuestro juego

--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -1,8 +1,12 @@
 import junit.framework.TestCase;
 
 public class BowlingGameTest extends TestCase {
+    private Game g;
+    protected  void setUp(){
+        g=new Game();
+    }
     public void testGutterGame()throws Exception{
-        Game g =new Game();
+   
         for(int i=0;i<20;i++){
             g.roll(0);
         }
@@ -10,12 +14,11 @@ public class BowlingGameTest extends TestCase {
     }
 
     public void testAllOnes()throws Exception{
-        Game g =new Game();
+
         for(int i=0;i<20;i++){
             g.roll(1);
         }
         assertEquals(20, g.score());
-
     }
 
 
