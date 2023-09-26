4e0ee1a2d821df845b3f90b4298a4b27a1551943
Carlos Sejas <carlos.sejas@ucb.edu.bo>
2023-09-26 01:23:27 -0400

    refactoring creamos metodo rollMany que simula nuestro lanzamientos

--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -6,22 +6,21 @@ public class BowlingGameTest extends TestCase {
         g=new Game();
     }
     public void testGutterGame()throws Exception{
-        int n=20;
-        int pins=0;
-   
-        for(int i=0;i<n;i++){
-            g.roll(pins);
-        }
+        rollMany(20,0);
         assertEquals(0, g.score());
     }
 
     public void testAllOnes()throws Exception{
 
-        for(int i=0;i<20;i++){
-            g.roll(1);
-        }
+        rollMany(20, 1);
         assertEquals(20, g.score());
     }
+     private void rollMany(int n, int pins){
+        for(int i=0;i<n;i++){
+            g.roll(pins);
+        }
+
+     }
 
 
 }
