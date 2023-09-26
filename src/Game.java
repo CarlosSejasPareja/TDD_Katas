8781e53caf35b1dd9dc04bebf75fd8054a045947
Carlos Sejas <carlos.sejas@ucb.edu.bo>
2023-09-26 01:53:54 -0400

    refactoring, mejora de calculo de bonus de spare

--- src/Game.java
+++ src/Game.java
@@ -14,7 +14,7 @@ public class Game {
                score+=10+strikeBonus(frameIndex);
                frameIndex++; 
             }else if(isSpare(frameIndex)){
-                score+=10+rolls[frameIndex+2];
+                score+=10+spareBonus(frameIndex);
                 frameIndex+=2;
             }else{
                score+=rolls[frameIndex]+rolls[frameIndex+1];
@@ -33,5 +33,9 @@ public class Game {
     private int strikeBonus(int frameIndex){
         return rolls[frameIndex+1]+rolls[frameIndex+2];
     }
+     private int spareBonus(int frameIndex){
+        return rolls[frameIndex+2];
+    }
+
 
 }
